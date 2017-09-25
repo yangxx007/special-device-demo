e5512c51df443c334fa59858681617501ade8b64
mariolew <mariolew@163.com>
2017-09-25 21:40:35 +0800

    deal with exception when redis connection fail

---
+++ src/main/java/com/example/demo/config/AppCacheConfigurer.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+//@Configuration
+//@EnableCaching
+public class AppCacheConfigurer extends CachingConfigurerSupport {
+
+    private Logger logger = LoggerFactory.getLogger(AppCacheConfigurer.class);
+
+    public class AppCacheErrorHandler implements CacheErrorHandler {
+
+        @Override
+        public void handleCacheGetError(RuntimeException e, Cache cache, Object o) {
+
+            if(e instanceof JedisConnectionException || e instanceof RedisConnectionFailureException){
+                logger.warn("redis has lose connection:",e);
+                return;
+            }
+            throw e;
+        }
+
+        @Override
+        public void handleCachePutError(RuntimeException e, Cache cache, Object o, Object o1) {
+            if(e instanceof JedisConnectionException || e instanceof RedisConnectionFailureException){
+                logger.warn("redis has lose connection:",e);
+                return;
+            }
+            throw e;
+        }
+
+        @Override
+        public void handleCacheEvictError(RuntimeException e, Cache cache, Object o) {
+            throw e;
+        }
+
+        @Override
+        public void handleCacheClearError(RuntimeException e, Cache cache) {
+            throw e;
+        }
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new AppCacheErrorHandler();
+    }
+
+}
--- src/main/java/com/example/demo/config/SpringRedisCacheManager.java
+++ src/main/java/com/example/demo/config/SpringRedisCacheManager.java
@@ -1,6 +1,9 @@
 package com.example.demo.config;
 
 import com.example.demo.config.annotation.CacheDuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
@@ -10,12 +13,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 import static com.google.common.collect.Sets.newHashSet;
 import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
@@ -24,6 +29,111 @@ import static org.springframework.util.StringUtils.isEmpty;
 public class SpringRedisCacheManager extends RedisCacheManager implements ApplicationContextAware, InitializingBean {
     private ApplicationContext applicationContext;
     private long defaultExpireTime;
+    private static Logger logger = LoggerFactory.getLogger(SpringRedisCacheManager.class);
+
+
+
+    @Override
+    public Cache getCache(String name) {
+        return new RedisCacheWrapper(super.getCache(name));
+    }
+
+
+    protected static class RedisCacheWrapper implements Cache {
+
+        private final Cache delegate;
+
+        public RedisCacheWrapper(Cache redisCache) {
+            Assert.notNull(redisCache, "delegate cache must not be null");
+            this.delegate = redisCache;
+        }
+
+        @Override
+        public String getName() {
+            try {
+                return delegate.getName();
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public Object getNativeCache() {
+            try {
+                return delegate.getNativeCache();
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public Cache.ValueWrapper get(Object key) {
+            try {
+                return delegate.get(key);
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public <T> T get(Object o, Class<T> aClass) {
+            try {
+                return delegate.get(o, aClass);
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public <T> T get(Object o, Callable<T> callable) {
+            try {
+                return delegate.get(o, callable);
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public void put(Object key, Object value) {
+            try {
+                delegate.put(key, value);
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+
+        @Override
+        public ValueWrapper putIfAbsent(Object o, Object o1) {
+            try {
+                return delegate.putIfAbsent(o, o1);
+            } catch (Exception e) {
+                return handleException(e);
+            }
+        }
+
+        @Override
+        public void evict(Object o) {
+            try {
+                delegate.evict(o);
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+
+        @Override
+        public void clear() {
+            try {
+                delegate.clear();
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+
+        private <T> T handleException(Exception e) {
+            logger.error("handleException", e);
+            return null;
+        }
+    }
     public SpringRedisCacheManager(RedisOperations redisOperations) {
         super(redisOperations);
     }
